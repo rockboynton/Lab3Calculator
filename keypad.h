a745e68fe903673f782811f032dab9bc5b6777c0
rockboynton <rock.boynton@yahoo.com>
2018-12-17 20:43:55 -0600

    keypad.h interface done but still need to add defines

--- keypad.h
+++ keypad.h
@@ -10,7 +10,39 @@
 
 #include <inttypes.h>
 
+/**
+ * Initialize the keypad GPIO port.
+ */
+void key_init();
 
+/**
+ * Returns a numeric code representing the button on the keypad that was pressed (1 to 16), or 0 if 
+ * no button is pressed. 
+ * 
+ * @return uint8_t 
+ *      numeric code representing the button on the keypad that was pressed
+ */
+uint8_t key_getKey_noBlock();
+
+/**
+ * Waits until a key on the keypad is pressed then returns the numeric code representing it (1 to 
+ * 16), or 0 if no button is pressed. 
+ * Returns after said key is released.
+ * 
+ * @return uint8_t 
+ *      numeric code representing the button on the keypad that was pressed
+ */
+uint8_t key_getKey();
+
+/**
+ * Waits until a key on the keypad is pressed then returns the ASCII code corresponding to the key 
+ * press.
+ * Returns after said key is released.
+ * 
+ * @return uint8_t 
+ *      ASCII code corresponding to the key press
+ */
+uint8_t key_getChar();
 
 
 #endif
