161abc36594eb34ee0425ce0db4e91d58711b082
rockboynton <rock.boynton@yahoo.com>
2018-12-19 00:43:10 -0600

    gpio.h and skeletons for lcd.c and keypad.c added

--- keypad.c
+++ keypad.c
@@ -11,3 +11,22 @@
 #include "delay.h"
 #include <inttypes.h>
 #include <stdio.h>
+#include <gpio.h>
+
+
+// Pointer to the GPIO structure
+static volatile GPIO* GPIOC = 0x40020800;
+
+void key_init() {
+    // TODO
+}
+
+uint8_t key_getKey_noBlock() {
+    return 0; // TODO
+}
+uint8_t key_getKey() {
+    return 0; // TODO
+}
+uint8_t key_getChar() {
+    return 0; // TODO
+}
--- lcd.c
+++ lcd.c
@@ -11,5 +11,35 @@
 #include "delay.h"
 #include <inttypes.h>
 #include <stdio.h>
+#include <gpio.h>
 
 
+// Pointer to the GPIO structure
+static volatile GPIO* GPIOC = 0x40020800;
+
+// Maintain current cursor index from ??? - ???
+static uint8_t currentIndex = 0;
+
+void lcd_init() {
+    // TODO
+}
+
+void lcd_clear() {
+    // TODO
+}
+
+void lcd_home() {
+    // TODO
+}
+
+void lcd_set_position(uint8_t row, uint8_t col) {
+    // TODO
+}
+
+uint32_t lcd_print_string(uint32_t str_ptr) {
+    return 0; // TODO
+}
+
+uint32_t lcd_print_num(uint32_t num) {
+    // TODO
+}
