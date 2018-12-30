a23567fa96ccee181883e8ad79413a320f04695b
Rock Boynton <boyntonrl@msoe.edu>
2018-12-29 23:59:24 -0600

    works just have to add divide by zero cond

--- main.c
+++ main.c
@@ -28,40 +28,45 @@
 // 2 = LCD tests
 #define DEBUG 0
 
+// Initialize variables to be used 
+static char TEMPLATE[] = "1 + 1 = __";
+uint8_t numChars;
+static const uint8_t A_INDEX = 0;
+static const uint8_t OP_INDEX = 2;
+static const uint8_t B_INDEX = 4;
+static const uint8_t RES_INDEX = 8;
+static const uint8_t KEY_MAP[] = {
+	0,1,2,3,4,5,6,7,8,9,
+	'+','-','x','/','*','#',0 // Key A->+, Key B-> -, Key C->x, Key D->/
+	};
+
+// Operands
+uint8_t a = 1;
+uint8_t b = 1;
+
+// Operator
+uint8_t op = '+';
+
+// Result
+uint8_t res;
+
+uint8_t result[2];
+
+// Valid flag
+uint8_t valid;
+
+// Current key
+uint8_t key;
+
+// Helper Functions
+static uint8_t calculate(uint8_t a, uint8_t op, uint8_t b);
+static void clear_res();
+static void enter();
+
 
 // main
 int main(){
-
-	// Initialize variables to be used 
-	static char TEMPLATE[] = "1 + 1 = __";
-	uint8_t numChars;
-	static const uint8_t A_INDEX = 0;
-	static const uint8_t OP_INDEX = 2;
-	static const uint8_t B_INDEX = 4;
-	static const uint8_t RES_INDEX = 8;
-	static const uint8_t KEY_MAP[] = {
-		0,1,2,3,4,5,6,7,8,9,
-		'+','-','x','/','*','#',0 // Key A->+, Key B-> -, Key C->x, Key D->/
-		};
-
-	// Operands
-	uint8_t a = 1;
-	uint8_t b = 1;
-
-	// Operator
-	uint8_t op = '+';
-
-	// Result
-	uint8_t res;
-
-	uint8_t result[2];
-
-	// Valid flag
-	uint8_t valid;
-
-	uint8_t position;
 	
-
 	// Initialize hardware
 	init_usart2(19200,F_CPU);
 	led_init();
@@ -70,11 +75,12 @@ int main(){
 
 	uint8_t key;
 
+
 	// --------------------- KEYPAD TEST CODE -------------------
 	if (DEBUG == 2) {
 		while (1) {
-			key = key_getKey();
-			lcd_print_num(key);
+			key = KEY_MAP[key_getKey()];
+			lcd_write_data(key);
 		}
 	}
 	//-----------------------------------------------------------
@@ -111,44 +117,73 @@ int main(){
 	lcd_print_string(TEMPLATE);
 	while (1) {
 		lcd_set_position(0, A_INDEX);
-		valid = 0;
 		// get valid input for operand a, or clear result/enter
+		valid = 0;
 		while (!valid) {
-			a = key_getKey();
-			if (a < 10 || a == '*' || a == '#') {
+			key = KEY_MAP[key_getKey()];
+			if (key < 10 || key == '*' || key == '#') {
 				valid = 1;
 			}
 		}
 		// a is valid
-		if (a == '*') { 
+		if (key == '*') { 
 			clear_res();
 			continue;
-		} else if (a == '#') {
+		} else if (key == '#') {
 			enter();
 			continue;
 		} else {
+			a = key;
 			lcd_set_position(0, A_INDEX);
 			lcd_print_num(a);
 		}
+
+		lcd_set_position(0, OP_INDEX);
 		// get valid input for operator, or clear result/enter
 		valid = 0;
 		while (!valid) {
-			b = key_getKey();
-			if (b < 10 || a == '*' || a == '#') {
+			key = KEY_MAP[key_getKey()];
+			if (key == '+' || key == '-' || key == 'x' || key == '/' || key == '*' || key == '#') {
+				valid = 1;
+			}
+		}
+		// op is valid
+		if (key == '*') { 
+			clear_res();
+			continue;
+		} else if (key == '#') {
+			enter();
+			continue;
+		} else {
+			op = key;
+			lcd_set_position(0, OP_INDEX);
+			lcd_write_data(op);
+		}
+
+		
+		
+		lcd_set_position(0, B_INDEX);
+		// get valid input for operand b, or clear result/enter
+		valid = 0;
+		while (!valid) {
+			key = KEY_MAP[key_getKey()];
+			if (key < 10 || key == '*' || key == '#') {
 				valid = 1;
 			}
 		}
 		// b is valid
-		if (b == '*') { 
+		if (key == '*') { 
 			clear_res();
 			continue;
-		} else if (b == '#') {
+		} else if (key == '#') {
 			enter();
 			continue;
 		} else {
-			lcd_set_position(0, OP_INDEX);
-			lcd_print_num();
+			b = key;
+			lcd_set_position(0, B_INDEX);
+			lcd_print_num(b);
 		}
+
 	}
 
 	// Never returns
@@ -177,13 +212,13 @@ static uint8_t calculate(uint8_t a, uint8_t op, uint8_t b) {
 	return res;
 }
 
-static uint8_t clear_res() {
+static void clear_res() {
 	lcd_set_position(0, RES_INDEX);
 	lcd_print_string("__");
 	lcd_home();
 }
 
-static uint8_t enter() {
+static void enter() {
 	lcd_set_position(0, RES_INDEX);
 	res = calculate(a, op, b);
 	sprintf(result, "%d", res);
